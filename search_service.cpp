a13726f4a042e41a2a60d98f48ede9418b4d9526
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-11-06 21:58:55 +0200

    search service: run_search gets scenario and prints it

--- search_service.cpp
+++ search_service.cpp
@@ -9,14 +9,12 @@ Searches::SearchService::SearchService(
     : scenario_service(scenario_service) {};
 
 void Searches::SearchService::run_search(int index) {
-  Scenarios::Scenario scenario = scenario_service.get_scenario(index);
-
-  Scenarios::Scenario scen = scenario_service.get_scenario(3, 2);
+  Scenarios::Scenario scen = scenario_service.get_scenario(index);
   std::cout << scen.id << " start:" << scen.start_x << "," << scen.start_y
             << ", goal:" << scen.goal_x << "," << scen.goal_y
             << ", cost:" << scen.cost << std::endl;
 }
 void Searches::SearchService::run_search(int bucket, int index) {
-  Searches::SearchService::run_search(100 * bucket + index);
+  Searches::SearchService::run_search(10 * bucket + index);
 }
 
