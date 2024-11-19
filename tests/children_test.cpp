38d985cd893881c742df490c898b720630d60e6b
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-11-19 21:42:48 +0200

    test map edges

--- tests/children_test.cpp
+++ tests/children_test.cpp
@@ -1,11 +1,28 @@
 #include "searches_common.h"
+#include "gtest/gtest.h"
 #include <gtest/gtest.h>
 #include <vector>
 #include <string>
 
 namespace {
 
-TEST(children, gives_8_back_on_empty) {
+
+std::vector<std::tuple<int, int, double>> vec2{
+    {1, 0, 1.0},
+    {0, 0, DIAG},
+    {0, 1, 1.0},
+    {0, 2, DIAG},
+    {1, 2, 1.0},
+    {2, 2, DIAG},
+    {2, 1, 1.0},
+    {2, 0, DIAG}
+  };
+
+
+
+
+// all are simple wrappers so imma just test them all indiscriminately.
+TEST(children_with_nodes, gives_8_back_on_empty) {
   std::vector<std::string> empty_map{"...", "...", "..."};
   std::vector<Node> vec;
   children(1, 1, empty_map, vec);
@@ -32,10 +49,135 @@ TEST(children, gives_8_back_on_empty) {
   // can do this too now it that it works:
   EXPECT_EQ(vec, vec2);
 }
+// TEST(children, test_edges) {
+// std::tuple<int, int, std::tuple<std::tuple<int, int, double>>> scens = {
+//     {0,1, {{0, 0, 1.0},{0, 2, 1.0},{1, 2, DIAG},{1, 1, 1.0},{1, 0, DIAG}}}
+//   };
+// }
 
-
-TEST(children, map_edges_respected) {
+TEST(children_with_tuple, map_edges_respected_W) {
   std::vector<std::string> le_map{"...","...","..."};
-  std::vector<Node> vec2;
+  std::vector<std::tuple<int, int, double>> node_list;
+  children(0, 1, le_map, node_list); 
+  EXPECT_EQ(node_list.size(), 5);
+  std::vector<std::tuple<int, int, double>> vec2{
+    {0, 0, 1.0},
+    {0, 2, 1.0},
+    {1, 2, DIAG},
+    {1, 1, 1.0},
+    {1, 0, DIAG}
+  };
+  EXPECT_EQ(node_list, vec2);
 }
+
+TEST(children_with_tuple, map_edges_respected_E) {
+  std::vector<std::string> le_map{"...","...","..."};
+  std::vector<std::tuple<int, int, double>> node_list;
+  children(2, 1, le_map, node_list); 
+  EXPECT_EQ(node_list.size(), 5);
+
+std::vector<std::tuple<int, int, double>> vec2{
+    {2, 0, 1.0},
+    {1, 0, DIAG},
+    {1, 1, 1.0},
+    {1, 2, DIAG},
+    {2, 2, 1.0},
+  };
+
+  EXPECT_EQ(node_list, vec2);
+}
+
+TEST(children_with_tuple, map_edges_respected_N) {
+  std::vector<std::string> le_map{"...","...","..."};
+  std::vector<std::tuple<int, int, double>> node_list;
+  children(1, 0, le_map, node_list); 
+  EXPECT_EQ(node_list.size(), 5);
+
+  std::vector<std::tuple<int, int, double>> vec2{
+    {0, 0, 1.0},
+    {0, 1, DIAG},
+    {1, 1, 1.0},
+    {2, 1, DIAG},
+    {2, 0, 1.0},
+  };
+
+  EXPECT_EQ(node_list, vec2);
+}
+
+TEST(children_with_tuple, map_edges_respected_S) {
+  std::vector<std::string> le_map{"...","...","..."};
+  std::vector<std::tuple<int, int, double>> node_list;
+  children(1, 2, le_map, node_list); 
+  EXPECT_EQ(node_list.size(), 5);
+  std::vector<std::tuple<int, int, double>> vec2{
+    {1, 1, 1.0},
+    {0, 1, DIAG},
+    {0, 2, 1.0},
+    {2, 2, 1.0},
+    {2, 1, DIAG}
+  };
+
+  EXPECT_EQ(node_list, vec2);
+}
+
+
+TEST(children_with_tuple, map_edges_respected_NW) {
+  std::vector<std::string> le_map{"...","...","..."};
+  std::vector<std::tuple<int, int, double>> node_list;
+  children(0, 0, le_map, node_list); 
+  EXPECT_EQ(node_list.size(), 3);
+  std::vector<std::tuple<int, int, double>> vec2{
+    {0, 1, 1.0},
+    {1, 1, DIAG},
+    {1, 0, 1.0},
+  };
+
+  EXPECT_EQ(node_list, vec2);
+}
+
+
+TEST(children_with_tuple, map_edges_respected_SW) {
+  std::vector<std::string> le_map{"...","...","..."};
+  std::vector<std::tuple<int, int, double>> node_list;
+  children(0, 2, le_map, node_list); 
+  EXPECT_EQ(node_list.size(), 3);
+  std::vector<std::tuple<int, int, double>> vec2{
+    {0, 1, 1.0},
+    {1, 2, 1.0},
+    {1, 1, DIAG}
+  };
+
+  EXPECT_EQ(node_list, vec2);
+}
+
+
+TEST(children_with_tuple, map_edges_respected_SE) {
+  std::vector<std::string> le_map{"...","...","..."};
+  std::vector<std::tuple<int, int, double>> node_list;
+  children(2, 2, le_map, node_list); 
+  EXPECT_EQ(node_list.size(), 3);
+  std::vector<std::tuple<int, int, double>> vec2{
+    {2, 1, 1.0},
+    {1, 1, DIAG},
+    {1, 2, 1.0},
+  };
+
+  EXPECT_EQ(node_list, vec2);
+}
+
+
+TEST(children_with_tuple, map_edges_respected_NE) {
+  std::vector<std::string> le_map{"...","...","..."};
+  std::vector<std::tuple<int, int, double>> node_list;
+  children(2, 0, le_map, node_list); 
+  EXPECT_EQ(node_list.size(), 3);
+  std::vector<std::tuple<int, int, double>> vec2{
+    {1, 0, 1.0},
+    {1, 1, DIAG},
+    {2, 1, 1.0},
+  };
+
+  EXPECT_EQ(node_list, vec2);
+}
+
 }// namespace ends
