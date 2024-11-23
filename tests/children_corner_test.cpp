ad27c4d3686339925d55588c1c5207467eae2692
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-11-23 09:17:33 +0200

    some tests, cleaned up file

--- tests/children_corner_test.cpp
+++ tests/children_corner_test.cpp
@@ -24,8 +24,6 @@ struct ChildrenTestCase {
   std::vector<std::tuple<int, int, double>> results;
 
   ChildrenTestCase(std::vector<std::string> i_map, std::string i_bools) : map_input(i_map), bools(i_bools) {};
-  // initializer_list constructor
-  ChildrenTestCase(std::vector<std::string> i_map, std::initializer_list<std::tuple<int, int, double>> i_results) : map_input(i_map), results(i_results) {};
   ChildrenTestCase() {};
 };
 
@@ -33,7 +31,6 @@ struct ChildrenTestCase {
 std::vector<std::tuple<int, int, double>> vec_for(std::string bools) {
   std::vector<std::tuple<int, int, double>> returnable;
   for (int i=0; i < bools.size(); i++) {
-    // std::cout << bools[i] << std::endl;
     if (bools[i] == '1') {
       returnable.push_back(vec2[i]);
     }
@@ -54,37 +51,11 @@ std::initializer_list<std::tuple<int, int, double>> full_list = {
       {2, 0, DIAG}
   };
 TEST(children_with_nodes, blocker_corner) {
-  std::vector<std::string> le_map1{"#..", "...", "..."};
-  std::vector<std::string> le_map2{"..#", "...", "..."};
-  std::vector<std::string> le_map3{"...", "...", "#.."};
-  std::vector<std::string> le_map4{"...", "...", "..#"};
-  ChildrenTestCase({"#..", "...", "..."}, {{1, 0, 1.0}});
-}
-// init_list ends
-
-//
-TEST(children_with_nodes, blocked_straight) {
-  std::vector<std::string> le_map1{".#.", "...", "..."};
-  std::vector<std::string> le_map2{"...", "#..", "..."};
-  std::vector<std::string> le_map3{"...", "..#", "..."};
-  std::vector<std::string> le_map4{"...", "...", ".#."};
-}
-
-
-TEST(children_with_nodes, blocked_straight2) {
-  std::vector<std::vector<std::string>> le_maps = {
-    {".#.", "...", "..."}, 
-    {"...", "#..", "..."},
-    {"...", "..#", "..."}, 
-    {"...", "...", ".#."}};
-}
-
-TEST(children_with_nodes, blocked_straight3) {
   std::vector<ChildrenTestCase> testcases = {
-    { {".#.", "...", "..."}, "00111110" },
-    { {"...", "#..", "..."}, "10001111" },
-    { {"...", "..#", "..."}, "11111000" },
-    { {"...", "...", ".#."}, "11100011" }
+    { {"#..", "...", "..."}, "10111111" },
+    { {"..#", "...", "..."}, "11111110" },
+    { {"...", "...", "#.."}, "11101111" },
+    { {"...", "...", "..#"}, "11111011" }
   };
   std::vector<std::tuple<int, int, double>> node_list;
   for (ChildrenTestCase tc : testcases) {
@@ -94,18 +65,33 @@ TEST(children_with_nodes, blocked_straight3) {
   }
 }
 
+TEST(children_with_nodes, blocked_straight) {
+  std::vector<ChildrenTestCase> testcases = {
+    { {".#.", "...", "..."}, "00111110" },
+    { {"...", "#..", "..."}, "10001111" },
+    { {"...", "..#", "..."}, "11111000" },
+    { {"...", "...", ".#."}, "11100011" }
+  };
+
+  std::vector<std::tuple<int, int, double>> node_list;
+  for (ChildrenTestCase tc : testcases) {
+    node_list.clear();
+    children(1,1, tc.map_input, node_list);
+    EXPECT_EQ(node_list, vec_for(tc.bools));
+  }
+}
 
 TEST(children_with_nodes, alley_between_blocked_corners) {
-  std::vector<std::string> le_map1{"#.#", "...", "..."};
-  std::vector<std::string> le_map2{"#..", "...", "#.."};
-  std::vector<std::string> le_map3{"...", "...", "#.#"};
-  std::vector<std::string> le_map4{"..#", "...", "..#"};
+  std::vector<ChildrenTestCase> testcases = {
+    { { "#.#", "...", "..."}, "10111110" },
+    { { "#..", "...", "#.."}, "10101111" },
+    { { "...", "...", "#.#"}, "11101011" },
+    { { "..#", "...", "..#"}, "11111010" }
+  };
+  std::vector<std::tuple<int, int, double>> node_list;
+  for (ChildrenTestCase tc : testcases) {
+    node_list.clear();
+    children(1,1, tc.map_input, node_list);
+    EXPECT_EQ(node_list, vec_for(tc.bools));
+  }
 }
-
-TEST(children_with_nodes, blocked_straight4) {
-  std::vector<std::string> le_map1{".#.", "...", "..."};
-  std::vector<std::string> le_map2{"...", "#..", "..."};
-  std::vector<std::string> le_map3{"...", "..#", "..."};
-  std::vector<std::string> le_map4{"...", "...", ".#."};
-}
-
