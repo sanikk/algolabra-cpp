8476c86dd0f01dbabb26e04e669f57cc4d200d0a
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-11-27 20:20:16 +0200

    common

--- src/searches/common/retval.h
+++ src/searches/common/retval.h
@@ -9,6 +9,6 @@ struct RetVal {
   std::vector<std::pair<int, int>> route;
 
   RetVal(bool i_found=false) : found(i_found) {}
-  RetVal(double i_cost, std::vector<int> i_route) : found(true), cost(i_cost), route(pair_route(i_route)) {}
+  RetVal(double i_cost, std::vector<int> i_route, int map_size) : found(true), cost(i_cost), route(pair_route(i_route, map_size)) {}
   RetVal(double i_cost, std::vector<std::pair<int, int>> i_route) : found(true), cost(i_cost), route(i_route) {}
 };
--- src/searches/common/tools.h
+++ src/searches/common/tools.h
@@ -7,5 +7,5 @@
 int xy2int(int x, int y, int map_size);
 std::pair<int, int> int2xy(int index, int map_size);
 
-std::vector<std::pair<int, int>> pair_route(std::vector<int> route);
-std::vector<int> unpair_route(std::vector<std::pair<int, int>> route);
+std::vector<std::pair<int, int>> pair_route(std::vector<int> route, int map_size);
+std::vector<int> unpair_route(std::vector<std::pair<int, int>> route, int map_size);
