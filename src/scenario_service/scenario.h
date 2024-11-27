9cde1d590edbe22c740df5faf4055819cdb6ecb3
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-11-27 17:47:55 +0200

    scenario: stream insertion operator<<.

--- src/scenario_service/scenario.h
+++ src/scenario_service/scenario.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <ostream>
 struct Scenario {
   int id;
   int start_x;
@@ -7,4 +8,9 @@ struct Scenario {
   int goal_x;
   int goal_y;
   float cost;
+
+
+  friend std::ostream& operator<<(std::ostream& os, const Scenario& obj) {
+    return os << "Scenario id:" << obj.id << ", start(" << obj.start_x << "," << obj.start_y << "), goal(" << obj.goal_x << "," << obj.goal_y << "), cost: " << obj.cost << std::endl; 
+  }
 };
