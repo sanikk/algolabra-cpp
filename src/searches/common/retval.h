4f33f885d3c6df37c0aa187c968f7e47d5b84a77
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-11-27 19:49:38 +0200

    applied in retval

--- src/searches/common/retval.h
+++ src/searches/common/retval.h
@@ -6,9 +6,9 @@
 struct RetVal {
   bool found;
   double cost;
-  std::vector<int> route;
+  std::vector<std::pair<int, int>> route;
 
   RetVal(bool i_found=false) : found(i_found) {}
-  RetVal(double i_cost, std::vector<int> i_route) : found(true), cost(i_cost), route(i_route) {}
-  RetVal(double i_cost, std::vector<std::pair<int, int>> i_route) : found(true), cost(i_cost) {}
+  RetVal(double i_cost, std::vector<int> i_route) : found(true), cost(i_cost), route(pair_route(i_route)) {}
+  RetVal(double i_cost, std::vector<std::pair<int, int>> i_route) : found(true), cost(i_cost), route(i_route) {}
 };
